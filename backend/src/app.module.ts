04830bc96a87d62219500c78be3f6aa7f87240d5
sjpeeris085 <sjpeeris.sl@gmail.com>
2026-03-31 22:56:09 +0530

    add github actions deployment script

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
+import * as dotenv from 'dotenv';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -14,19 +15,30 @@ import { NotificationsModule } from '@modules/notifications/notifications.module
 import { LoggingInterceptor } from '@common/interceptors/logging.interceptor';
 import { UsersModule } from '@modules/users/users.module';
 
+dotenv.config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: 'postgres', // process.env.DB_USERNAME,
-      password: 'root', //process.env.DB_PASSWORD,
-      database: 'backend_assessment', //process.env.DB_NAME,
-
+      // host: process.env.DB_HOST,
+      // port: 5432,
+      host: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+      username: process.env.DB_USERNAME, // 'postgres',
+      password: process.env.DB_PASSWORD, // 'root'
+      database: process.env.DB_NAME, // 'backend_assessment'
       // entities: [Product],
       autoLoadEntities: true,
-      synchronize: true, // dev only
+      // synchronize: true, // dev only
+      synchronize: process.env.NODE_ENV !== 'production',
+
+      // Google Cloud Production
+      extra:
+        process.env.NODE_ENV === 'production'
+          ? {
+              host: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+            }
+          : {},
     }),
     ProductsModule,
     OrdersModule,
